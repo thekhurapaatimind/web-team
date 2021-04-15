d09053415dfc9f0ec4962173640c259ba6a2011d
thekhurapaatimind <79720859+thekhurapaatimind@users.noreply.github.com>
2021-04-15 21:20:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
+    
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         burger.classList.toggle('toggle');
@@ -12,7 +13,7 @@ const navSlide = () => {
                 link.style.animation = '';
             }
             else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
             }        
 
         });
@@ -21,5 +22,4 @@ const navSlide = () => {
     });
 
 }  
-
 navSlide(); 
